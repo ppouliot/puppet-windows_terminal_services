ef9185a88887c05275a43c0c08ecad8b8a18cf1b
primeministerp <primeministerpete@gmail.com>
2016-04-06 11:35:08 -0400

    trying to fix typo

--- manifests/init.pp
+++ manifests/init.pp
@@ -23,16 +23,12 @@
 #
 # Copyright 2015 Peter J. Pouliot <peter@pouliot.net>, unless otherwise noted.
 
-class windows_terminal_services ( $remote_access ){
-
+class windows_terminal_services ($remote_access){
   validate_re($remote_access, '^(enable|disable)$', 'valid values for ensure are \'enable\' or \'disable\'')
-
   $ts_deny_connections = $remote_access ? {
     (/enable/)  => '0',
     (/disable/) => '1',
-    default     => undef,
   }
-
   if $kernel == 'windows' {
     registry_key {'HKLM\SYSTEM\CurrentControlSet\Control\Terminal Server':
       ensure => present,
