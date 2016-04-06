6d4a6ac715756c813d2fc0940855654904a24837
primeministerp <primeministerpete@gmail.com>
2016-04-06 12:05:44 -0400

    changing technique for troubled lines

--- manifests/init.pp
+++ manifests/init.pp
@@ -22,23 +22,22 @@
 # === Copyright
 #
 # Copyright 2015 Peter J. Pouliot <peter@pouliot.net>, unless otherwise noted.
-
-class windows_terminal_services(
-  $remote_access,
-){
+class windows_terminal_services($remote_access){
   validate_re($remote_access, '^(enable|disable)$', 'valid values for ensure are \'enable\' or \'disable\'')
-  $ts_deny_connections = $remote_access ? {
-    (/enable/)  => '0',
-    (/disable/) => '1',
+  if $remote_access = 'enable' {
+    $ts_deny_connections = '0'
+  } else {
+    $ts_deny_connections = '1' 
   }
   if $kernel == 'windows' {
+    notice('Ensuring Registry key HKLM\SYSTEM\CurrentControlSet\Control\Terminal Server')
     registry_key {'HKLM\SYSTEM\CurrentControlSet\Control\Terminal Server':
       ensure => present,
-    }
+    } warning("${module_name} Ensuring Registry key HKLM\SYSTEM\CurrentControlSet\Control\Terminal Server is present")
     registry_value {'HKLM\SYSTEM\CurrentControlSet\Control\Terminal Server\fDenyTSConnection':
       type => string,
       data => $ts_deny_connections,
-    }
+    } warning("${module_name} Ensuring Registry key value HKLM\SYSTEM\CurrentControlSet\Control\Terminal Server\fDenyTSConnection is set to ${ts_deny_connections}")
   } else {
     fail("${module_name} is for Windows Platforms only")
   }
